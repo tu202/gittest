7d70facbd9fc83469099769fec1c1c74be0aed23
testuser <testuser@frogfly123.com>
2024-06-21 11:04:02 +0800

    2024-06-21 11:04:02

--- mygit.py
+++ mygit.py
@@ -5,7 +5,7 @@ import time
 repo = Repo.init('.')  #初始化
 repo = Repo('.')
 repo.index.add(['mygit.py'])  #檔案
-now=time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+now=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
 repo.index.commit(now)  #註解
 g = Git(repo.working_dir)
 g.push('https://github.com/tu202/gittest.git')
